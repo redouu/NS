efc831b2b9a516e031510baebda6a78a0942f3ba
shibuza <brahimasdaou@gmail.com>
2016-11-08 15:29:44 +0100

    Update reisinfo.py
    Comments toegevoegd

--- reisinfo.py
+++ reisinfo.py
@@ -4,3 +4,25 @@ import xmltodict
 from datetime import datetime, timedelta
 from tkinter import *
 from tkinter import messagebox
+
+#variable aanmaken met het pad naar xml bestand
+
+#toegang krijgen tot de API d.m.v. de volgende gegevens te defineren
+# 'brahim.asdaou@student.hu.nl',
+# 'KNZYQ_8TBr24vRmUm7UYUDDx_hz0ybxtAks_2_t5V1_eZvzwSM_RyQ'
+
+# functie scrijf_xml defineren
+
+# functie lees_ xml defineren
+
+# functie NS_API defineren met 2 parameters(reg, auth_details)
+
+# functie station ophalen defineren
+
+# functie station informatie ophalen defineren
+
+# functie scherm_init() defineren om alle informatie en grafische objecten weer te geven.
+# binnen de functie scherm_init() een andere functie defineren click()
+# click functie wordt uitgevoerd zodra de gebuiker op de button klikt.
+
+scherm_init().mainloop() # om een loop te maken van de programma zodat deze constant wordt weergegeven
