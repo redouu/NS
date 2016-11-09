39bf58510fad702381b16634d80c34d2ac6d5c96
shibuza <brahimasdaou@gmail.com>
2016-11-10 00:22:27 +0100

    Versie 0.3
    Grafische interface ontworpen en verbinding gemaakt met de NS API

--- reisinfo.py
+++ reisinfo.py
@@ -5,37 +5,62 @@ from datetime import datetime, timedelta
 from tkinter import *
 from tkinter import messagebox
 
-xml_file = 'reisinfo.xml'  #variable aanmaken met het pad naar xml bestand
-#defineren van de NS API authenticatie gegevens(gebruikersnaam en key)
-api_pass = ('brahim.asdaou@student.hu.nl', 'KNZYQ_8TBr24vRmUm7UYUDDx_hz0ybxtAks_2_t5V1_eZvzwSM_RyQ')
-
-def read_xml():  # functie read_xml defineren
-
-def write_xml(reply, file):  # functie write_xml defineren
-  bestand = open(file, 'w')  # bestand openen
-  bestand.write(str(reply))  # bestand schrijven
-  bestand.close()  # bestand afsluiten
+xml_file = 'reisinfo.xml'
+api_key = ('brahim.asdaou@student.hu.nl', 'KNZYQ_8TBr24vRmUm7UYUDDx_hz0ybxtAks_2_t5V1_eZvzwSM_RyQ')
+root = Tk()
+root.configure(background="#ffc917")
+root.attributes('-fullscreen', True)
+root.iconbitmap(r'ns.ico')
+def esc(event):
+        sys.exit()
+root.bind('<Escape>', esc)
+def lees_xml(file):
+    bestand = open(file, 'r')
+    xml_string = bestand.read()
+    return xmltodict.parse(xml_string)
+def write_xml(reply, file):
+    bestand = open(file, 'w')
+    bestand.write(str(reply))
+    bestand.close()
+def request_xml(http, key):
+    try:
+        reply = requests.get(http, auth=key)
+        return reply
+    except:
+        return logging.exception('')
+      
+# def station items
+# def station info
   
-# functie NS_API defineren met 2 parameters(reg, auth_details)
-
-# functie station ophalen defineren
-
-# functie station informatie ophalen defineren
-
-# functie scherm_init() defineren om alle informatie en grafische objecten weer te geven.
-# binnen de functie scherm_init() een andere functie defineren click()
-# click functie wordt uitgevoerd zodra de gebuiker op de button klikt.
-def scherm_init():
-  scherm = Tk()
-  scherm.wm_title("NS Reisinformatie")  # De Titel van het venseter scherm(verschijnt op de titelbalk)
-  scherm.iconbitmap(r'ns.ico')  # Het icoontje toevoegen(verschijnt op de titelbalk)
-  scherm.configure(background="#ffc917") # Achtergrond kleur
-  scherm.attributes('-fullscreen', True)  # Volledig scherm
-
-  def click(station): 
-    if len(station) == 0: #als er niks is ingevuld
-            msg = "Vul station in!" # melding defineren
-            messagebox.showinfo(title="Melding", message=msg) #melding laten zien
-    else:
-  
-scherm_init().mainloop() # om een loop te maken van de programma zodat deze constant wordt weergegeven
+def info_init():
+    root.wm_title("NS Reisinformatie")
+    #  Tekstlabel weergeven
+    infolabel = Label(root, text="Voer hieronder uw huidige station in om de actuele reistijden en overige reisinformatie op te vragen.", background="#ffc917", foreground="#003082", anchor=CENTER)
+    infolabel.config(font=("Arial", 16))
+    infolabel.place(relx=.5, rely=.02, anchor="center")
+    #  Input weergeven
+    input = Entry(root, width=42)
+    input.config(font=("Arial", 16))
+    input.place(relx=.5, rely=.05, anchor="center")
+    #  Button weergeven en linken naar de click functie
+    searchbutton = Button(root, text="Bekijk de actuele reisinformatie", background="#003082", foreground="white", command=lambda: click(input.get()), anchor=CENTER)
+    searchbutton.config(font=("Arial", 16))
+    searchbutton.place(relx=.5, rely=.09, anchor="center")
+    def click(station):
+        if len(station) == 0:
+            msg = "Vul station in!"
+            messagebox.showinfo(title="Melding", message=msg)
+        else:
+            if station:
+                try:
+                    msg = get_station_info(station)
+                    infotext = Label(root, height=56, width=64, text=msg, anchor=N, font=("Arial", 10, "bold"), background="#ffffff")
+                    infotext.place(relx=.5, rely=.54, anchor="center")
+                except UserWarning as msg:
+                    msg = 'Onjuiste Stationsnaam!'
+                    messagebox.showinfo(title="Melding", message=msg)
+            else:
+                msg = 'Er is een fout opgetreden..'
+                messagebox.showinfo(title="Melding", message=msg)
+    return root
+start_init().mainloop()
